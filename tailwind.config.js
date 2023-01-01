f3cf53dc89ed6003d797e950c0f86b3d594d998d
spatulatom <spatulatom@gmail.com>
2023-01-01 02:03:10 +0000

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        archivo: "Archivo",
-        latoLight: "LatoLight",
-        latoBold: "LatoBold",
-        latoReg: "LatoReg",
+        archivo: 'Archivo',
+        latoLight: 'LatoLight',
+        latoBold: 'LatoBold',
+        latoReg: 'LatoReg',
       },
+      color: {
+        'blackish': 'rgb(0, 0, 0, 0.7)'
+      },
+
       height: {
-        "10v": "10vh",
-        "20v": "20vh",
-        "30v": "30vh",
-        "40v": "40vh",
-        "50v": "50vh",
-        "60v": "60vh",
-        "70v": "70vh",
-        "80v": "80vh",
-        "90v": "90vh",
-        "100v": "100vh",
+        '10v': '10vh',
+        '20v': '20vh',
+        '30v': '30vh',
+        '40v': '40vh',
+        '50v': '50vh',
+        '60v': '60vh',
+        '70v': '70vh',
+        '80v': '80vh',
+        '90v': '90vh',
+        '100v': '100vh',
       },
-      width:{
-        "80v": "80vw"
-      }
-        },
+      width: {
+        '80v': '80vw',
+      },
+    },
   },
   plugins: [],
 };
