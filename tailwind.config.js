55f7bc3a15ae2daa6bf49d4bb5e3a1be0e52ea9e
spatulatom <spatulatom@gmail.com>
2023-01-08 15:39:49 +0000

    fgoogle fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,25 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // archivo: 'Archivo',
-        
+        sans: ['Poppins', 'sans-serif'],
       },
       colors: {
         blackish: 'rgb(0, 0, 0, 0.3)',
         customBlack: 'rgb(30, 30, 40)',
-        customGreen: '#6b706d'
+        customGreen: '#6b706d',
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%) ',
       },
-
-  
-      width: {
-        '80v': '80vw',
+      backgroundImage: {
+        road: "url('../imgs/road3.jpg')",
+      },
+      spacing: {
+        180: '32rem',
       },
     },
   },
