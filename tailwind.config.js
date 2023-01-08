f7e327e415f1228a9a50fdcaa1464fa04979139c
spatulatom <spatulatom@gmail.com>
2023-01-08 01:17:06 +0000

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,18 +15,7 @@ module.exports = {
         customGreen: '#6b706d'
       },
 
-      height: {
-        '10v': '10vh',
-        '20v': '20vh',
-        '30v': '30vh',
-        '40v': '40vh',
-        '50v': '50vh',
-        '60v': '60vh',
-        '70v': '70vh',
-        '80v': '80vh',
-        '90v': '90vh',
-        '100v': '100vh',
-      },
+  
       width: {
         '80v': '80vw',
       },
