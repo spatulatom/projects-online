66982af2e9817d277c68ef901734f9e18f5c74e5
spatulatom <spatulatom@gmail.com>
2023-05-17 12:21:48 +0100

    add date function

--- scripts/date.js
+++ scripts/date.js
@@ -1,12 +1,26 @@
+// const date = document.querySelector('.date');
+// const body = document.getElementById('body');
+
+// const addDate = () => {
+//   console.log('loaded');
+//   let newDate = new Date(new Date().setHours(new Date().getHours() - 24));
+//   localDate = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(
+//     newDate
+//   );
+//   date.innerHTML = localDate;
+// };
+// body.addEventListener('load', addDate());
+
+// chatGPT SUGGESTIONS:
 const date = document.querySelector('.date');
 const body = document.getElementById('body');
 
 const addDate = () => {
   console.log('loaded');
   let newDate = new Date(new Date().setHours(new Date().getHours() - 24));
-  localDate = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(
-    newDate
-  );
+  let localDate = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(newDate);
   date.innerHTML = localDate;
 };
-body.addEventListener('load', addDate());
+
+body.addEventListener('load', addDate);
+
