1f3dcb7be22073c75793c66f4c39b100636b62a8
spatulatom <spatulatom@gmail.com>
2023-06-06 10:29:05 +0100

    trying dynamic exports

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,14 @@
 import modes from "./dark-mode.js";
+import contactForm from "./contact-form.js";
 
 modes()
+contactForm()
+
+// trying dynamic import on hover over contact form
+// const formData = document.querySelector('.form');
+// formData.addEventListener('mouseover', ()=>{
+//     console.log('ONMOUSE')
+//     import('./contact-form.js').then((module)=>{const form =module.formData;
+//     form() })
+//     // await contactForm()
+// })
--- scripts/contact-form.js
+++ scripts/contact-form.js
@@ -1,65 +1,69 @@
 const formData = document.querySelector('.form');
 const results = document.querySelector('.form__feedback');
 
-const handleFormSubmit = async (event) => {
-  event.preventDefault();
-  console.log('results', results);
+function contactForm() {
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log('RESULTS', results);
 
-  const data = new FormData(event.target);
-  const object = {};
-  data.forEach((value, key) => {
-    object[key] = value;
-  });
+    const data = new FormData(event.target);
+    const object = {};
+    data.forEach((value, key) => {
+      object[key] = value;
+    });
 
-  if (object.email !== object.email2) {
-    results.innerHTML = 'Your email adresses are not matching!';
-    console.log(object, 'object');
-  } else {
-    delete object.email2;
-    const json = JSON.stringify(object);
-    console.log('json', json);
-    results.innerHTML = 'Please wait...';
+    if (object.email !== object.email2) {
+      results.innerHTML = 'Your email adresses are not matching!';
+      console.log(object, 'object');
+    } else {
+      delete object.email2;
+      const json = JSON.stringify(object);
+      console.log('json', json);
+      results.innerHTML = 'Please wait...';
 
-    try {
-      // const response = await fetch('http://localhost:5000/contact-form',{
-      const response = await fetch(
-        'https://my-portfolio-backend.onrender.com/contact-form',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: json,
-        }
-      );
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        console.log(jsonResponse, 'jsonResponse');
-        // results.innerHTML = jsonResponse.message;
+      try {
+        // const response = await fetch('http://localhost:5000/contact-form',{
+        const response = await fetch(
+          'https://my-portfolio-backend.onrender.com/contact-form',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: json,
+          }
+        );
+        if (response.ok) {
+          const jsonResponse = await response.json();
+          console.log(jsonResponse, 'jsonResponse');
+          // results.innerHTML = jsonResponse.message;
 
-        setTimeout(() => {
-          formData.reset();
-          results.innerHTML = jsonResponse.message;
-        }, 4000);
+          setTimeout(() => {
+            formData.reset();
+            results.innerHTML = jsonResponse.message;
+          }, 4000);
 
-        setTimeout(() => {
+          setTimeout(() => {
+            results.innerHTML =
+              'You will get a confirmation email from elkom9393@gmail.com (might be in the spam folder). Have a nice day! I will reply to you back soon!';
+          }, 6000);
+        } else {
+          const jsonResponse = await response.json();
+          console.log(jsonResponse.message, 'response');
           results.innerHTML =
-            'You will get a confirmation email from elkom9393@gmail.com (might be in the spam folder). Have a nice day! I will reply to you back soon!';
-        }, 6000);
-      } else {
-        const jsonResponse = await response.json();
-        console.log(jsonResponse.message, 'response');
+            'Error occured while processing your form data, try again please!';
+          formData.reset();
+        }
+      } catch (error) {
+        console.log(error, 'error');
         results.innerHTML =
-          'Error occured while processing your form data, try again please!';
+          'Something went wrong with the external server, try again in a minute please!';
         formData.reset();
       }
-    } catch (error) {
-      console.log(error, 'error');
-      results.innerHTML =
-        'Something went wrong with the external server, try again in a minute please!';
-      formData.reset();
     }
-  }
+  };
+
+  formData.addEventListener('submit', handleFormSubmit);
 };
 
-formData.addEventListener('submit', handleFormSubmit);
+export default contactForm;
