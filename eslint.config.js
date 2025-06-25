9bcb0c085d48b23eff2b8de320fa7ed298c4d3b2
spatulatom <spatulatom@gmail.com>
2025-06-25 23:05:46 +0100

    eslint can not read broeserslist through nither: "eslint-plugin-compat": "^6.0.2",     "eslint-plugin-es-x": "^8.7.0", for that reason it lints with deafult settings; then browserslits through  "browserslist-config-baseline": "^0.4.0", gets to pull baseline - that is pulled by postcss or babel (vite is using esbuild by deafult as babels is not set up, esbuild dosent read browserlist)

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,16 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import compatPlugin from "eslint-plugin-compat";
 
-export default defineConfig([
-  // JavaScript files
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  // JavaScript and TypeScript files - compat plugin
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: {
-      compat: compatPlugin,
-    },
-    rules: {
-      ...compatPlugin.configs.recommended.rules,
-      "compat/compat": "error",
-    },
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-    },
-  },
-  // TypeScript files
+export default [
+  js.configs.recommended,
   ...tseslint.configs.recommended,
-]);
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
